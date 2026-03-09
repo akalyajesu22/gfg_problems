24fe0e75146dcac0e0d726603054e6f9a5900c51
Akalya S <akalyajesu22@gmail.com>
2026-03-09 21:56:28 +0530

    Add files via upload

---
+++ Duplicatearray.java
@@ -0,0 +1,21 @@
+class Duplicatearray {
+    public static boolean hasDuplicate(int[] nums) {
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[i] == nums[j])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[]args)
+    {
+        int nums[] = { 1,2,3,3};
+        boolean b = hasDuplicate(nums);
+        System.out.print(b);
+    }
+}
