590260574687d952580225e2abb3579998f57506
Akalya S <akalyajesu22@gmail.com>
2026-03-10 21:43:58 +0530

    Add files via upload

---
+++ Checksubsequence.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class  Checksubsequence{
+    public  static boolean isSubsequence(String s, String t) {
+        int i=0, j=0;
+        while(i < s.length() && j < t.length())
+        {
+            if(s.charAt(i) == t.charAt(j))
+            {
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
+    }
+    public static void main(String[]args)
+    {
+        String s = "node";
+        String t = "neetcode";
+        boolean b = isSubsequence(s,t);
+        System.out.print(b);
+    }
+}
